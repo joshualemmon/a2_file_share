e03b60bf537c22c3b40be9fe0424e401cfdebd81
Josh Lemmon <joshua.lemmon@uoit.net>
2016-03-22 14:27:43 -0400

    In lieu of using command line arguments, program uses dialog boxes to prompt user for computer name and folder path

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,9 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SplitPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -16,6 +14,9 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+
+    private static String computerName = "";
+    private static String folderPath = "";
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -30,6 +31,24 @@ public class Main extends Application {
 
     public static void drawUI(Group root)
     {
+        TextInputDialog argsPrompt = new TextInputDialog();
+
+        argsPrompt.setHeaderText("Computer Name");
+        argsPrompt.setContentText("Please input your computer name");
+        do
+        {
+            argsPrompt.showAndWait();
+            computerName = argsPrompt.getEditor().getText();
+        }while(computerName.equals(""));
+
+        argsPrompt.setHeaderText("Folder Path");
+        argsPrompt.setContentText("Please input your local folder path");
+        do
+        {
+            argsPrompt.showAndWait();
+            folderPath = argsPrompt.getEditor().getText();
+        }while(folderPath.equals(""));
+        System.out.println(computerName + "    " + folderPath);
         BorderPane bp = new BorderPane();
         HBox h = new HBox();
 
@@ -53,14 +72,18 @@ public class Main extends Application {
         });
         ListView leftList = new ListView();
         ListView rightList = new ListView();
+
         SplitPane sp = new SplitPane();
+
         final StackPane sp1 = new StackPane();
         sp1.getChildren().addAll(leftList);
         final StackPane sp2 = new StackPane();
         sp2.getChildren().add(rightList);
+
         sp.getItems().addAll(sp1,sp2);
         sp.setMinHeight(465);
         sp.setDividerPosition(1, 0.5f);
+
         h.getChildren().addAll(downloadButton, uploadButton);
         bp.setTop(h);
         bp.setCenter(sp);
