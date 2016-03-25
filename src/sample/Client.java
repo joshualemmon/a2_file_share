20d443dd275688c0ed02463a67f43065aa54d5f9
wuwoot9863 <cs.tony.wu@gmail.com>
2016-03-25 19:53:49 -0400

    Adding the correct files work, however, the server continues on making empty threads. There will also be a null file that is created after uploading a file in the serverFolder. The socket is closed after one file is uploaded, therefore you can only upload one file every time you start the program. Needs fixing.

--- src/sample/Client.java
+++ src/sample/Client.java
@@ -1,5 +1,4 @@
-/*
-package sample;
+/*package sample;
 
 import java.io.*;
 import java.net.*;
@@ -7,34 +6,48 @@ import java.net.*;
 public class Client {
     protected BufferedInputStream bis   = null;
     protected BufferedOutputStream bos  = null;
-    protected FileOutputStream fos      = null;
-
-    //protected DataOutputStream dos  = null;
-    //protected BufferedReader in     = null;
 
     protected Socket clientSocket   = null;
     protected String localHost      = "127.0.0.1";
     protected int port              = 8080;
 
     protected File uploadFile       = null;
-    protected File serverDir        = null;
 
-
-    public Client(File uploadFile) {
-        this.uploadFile = uploadFile;
-        this.serverDir = new File("/home/wuwoot/Documents/b/serverFolder/" + uploadFile.getName());
+    public Client(String localHost, int port) {
+        this.localHost = localHost;
+        this.port = port;
 
         try {
             clientSocket = new Socket(localHost, port);
-
         } catch (IOException e) {
             System.err.println("IOEXception while creating server connection");
         }
     }
 
-    protected boolean setUpload(File uploadFile) {
-        return true;
+    public void uploading(File uploadFile)
+    {
+        this.uploadFile = uploadFile;
+
+        try
+        {
+            long lengthOfFile = uploadFile.length();    //Counts the length of the file
+            byte[] bytesInFile = new byte[(int)(lengthOfFile)]; //Stores the bytes in the file, I don't know how though
+
+            bis = new BufferedInputStream(new FileInputStream(uploadFile)); //Stream for file
+            bis.read(bytesInFile, 0, bytesInFile.length);                   //Read in the file to stream
+            bos = new BufferedOutputStream(clientSocket.getOutputStream()); //Stream to socket
+            bos.write(bytesInFile, 0, bytesInFile.length);                  //Write file data to socket
+            bos.flush(); //Finalize
+
+            bis.close();
+            bos.close();
+
+        } catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
     }
+}
 
 /*
     protected boolean processCommand(String command, String filename) {
@@ -55,9 +68,5 @@ public class Client {
         }
     }
 */
-/*
-    public static void main(String[] args) {
 
-    }
-}*/
 
